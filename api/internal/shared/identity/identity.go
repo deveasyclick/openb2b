0cf2223b128d7a01cc288bc710993048c846f635
Yusuf <easyclick06@gmail.com>
2025-08-24 21:51:32 +0100

    feat(auth); add identity package for retrieving custom claims from context

---
+++ api/internal/shared/identity/identity.go
@@ -0,0 +1,66 @@
+package identity
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/clerk/clerk-sdk-go/v2"
+	parseuint "github.com/deveasyclick/openb2b/internal/utils/parseUint"
+)
+
+type CustomSessionClaims struct {
+	OrgID  string `json:"org_id,omitempty"`
+	UserID string `json:"user_id,omitempty"`
+}
+
+type ContextUser struct {
+	ID  uint
+	Org uint
+}
+
+func GetCustomClaims(ctx context.Context) (*CustomSessionClaims, error) {
+	claims, ok := clerk.SessionClaimsFromContext(ctx)
+	if !ok {
+		return nil, fmt.Errorf("no session claims found in context")
+	}
+	customClaims, ok := claims.Custom.(*CustomSessionClaims)
+
+	if !ok {
+		return nil, fmt.Errorf("invalid or missing custom claims")
+	}
+
+	return &CustomSessionClaims{
+		UserID: customClaims.UserID,
+		OrgID:  customClaims.OrgID,
+	}, nil
+}
+
+func ClerkIDFromContext(ctx context.Context) (string, bool) {
+	claims, ok := clerk.SessionClaimsFromContext(ctx)
+	if !ok {
+		return "", false
+	}
+	return claims.Subject, false
+}
+
+func UserFromContext(ctx context.Context) (*ContextUser, error) {
+	claims, err := GetCustomClaims(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	userID, err := parseuint.ParseUint(claims.UserID, "user ID")
+	if err != nil {
+		return nil, err
+	}
+
+	orgID, err := parseuint.ParseUint(claims.OrgID, "org ID")
+	if err != nil {
+		return nil, err
+	}
+
+	return &ContextUser{
+		ID:  userID,
+		Org: orgID,
+	}, nil
+}
