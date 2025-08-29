8a3bf27d344b93156155e0bdc388277f58f2dd3f
Yusuf <easyclick06@gmail.com>
2025-08-29 17:11:13 +0100

    feat(product): make org nullable in product model

--- api/internal/model/product.go
+++ api/internal/model/product.go
@@ -1,16 +1,20 @@
 package model
 
+// Product represents an product entity
+// @Description Product response model
 type Product struct {
 	BaseModel
 	Name        string    `gorm:"not null" json:"name"`
 	Category    string    `json:"category"`
 	OrgID       uint      `gorm:"index;not null" json:"orgId"`
-	Org         Org       `gorm:"foreignKey:OrgID" json:"org,omitempty"`
+	Org         *Org      `gorm:"foreignKey:OrgID" json:"org,omitempty"`
 	ImageURL    string    `json:"imageUrl"`
 	Description string    `json:"description"`
 	Variants    []Variant `json:"variants" gorm:"foreignKey:ProductID"`
 }
 
+// Variant represents an variant entity
+// @Description Variant response model
 type Variant struct {
 	ID        uint `gorm:"primaryKey"`
 	ProductID uint `gorm:"index;not null"`
