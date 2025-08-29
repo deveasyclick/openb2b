0207288b1abb2800f584e58d4413244b422854c2
Yusuf <easyclick06@gmail.com>
2025-08-29 15:30:50 +0100

    feat(product): add dtos

---
+++ api/internal/modules/product/dto.go
@@ -0,0 +1,114 @@
+package product
+
+import "github.com/deveasyclick/openb2b/internal/model"
+
+type createProductVariantDTO struct {
+	SKU   string  `json:"sku" validate:"required,min=2,max=50"`
+	Color string  `json:"color" validate:"omitempty,min=1,max=30"`
+	Size  string  `json:"size" validate:"omitempty,min=1,max=30"`
+	Price float64 `json:"price" validate:"required,gt=0"`
+	Stock int     `json:"stock" validate:"required,min=0"`
+}
+
+type createProductDTO struct {
+	Name        string                    `json:"name" validate:"required,min=2,max=100"`
+	Category    string                    `json:"category" validate:"omitempty,min=2,max=50"`
+	ImageURL    string                    `json:"imageUrl" validate:"omitempty,url"`
+	Description string                    `json:"description" validate:"omitempty,min=2,max=1000"`
+	Variants    []createProductVariantDTO `json:"variants" validate:"required,dive"`
+	OrgID       uint                      `json:"orgId" validate:"required"`
+}
+
+func (p *createProductDTO) ToModel() model.Product {
+	product := model.Product{
+		Name:        p.Name,
+		Category:    p.Category,
+		ImageURL:    p.ImageURL,
+		Description: p.Description,
+		OrgID:       p.OrgID,
+	}
+
+	// map variants
+	for _, v := range p.Variants {
+		product.Variants = append(product.Variants, model.Variant{
+			SKU:   v.SKU,
+			Color: v.Color,
+			Size:  v.Size,
+			Price: v.Price,
+			Stock: v.Stock,
+			OrgID: p.OrgID, // enforce same org
+		})
+	}
+
+	return product
+}
+
+// ----- UPDATE -----
+
+type updateVariantDTO struct {
+	ID    *uint    `json:"id" validate:"omitempty"`
+	SKU   *string  `json:"sku" validate:"omitempty,min=2,max=50"`
+	Color *string  `json:"color" validate:"omitempty,min=1,max=30"`
+	Size  *string  `json:"size" validate:"omitempty,min=1,max=30"`
+	Price *float64 `json:"price" validate:"omitempty,gt=0"`
+	Stock *int     `json:"stock" validate:"omitempty,min=0"`
+}
+
+type updateProductDTO struct {
+	Name        *string            `json:"name" validate:"omitempty,min=2,max=100"`
+	Category    *string            `json:"category" validate:"omitempty,min=2,max=50"`
+	ImageURL    *string            `json:"imageUrl" validate:"omitempty,url"`
+	Description *string            `json:"description" validate:"omitempty,min=2,max=1000"`
+	Variants    []updateVariantDTO `json:"variants" validate:"omitempty,dive"`
+}
+
+func (dto *updateProductDTO) ApplyModel(product *model.Product) {
+	if dto.Name != nil {
+		product.Name = *dto.Name
+	}
+	if dto.Category != nil {
+		product.Category = *dto.Category
+	}
+	if dto.ImageURL != nil {
+		product.ImageURL = *dto.ImageURL
+	}
+	if dto.Description != nil {
+		product.Description = *dto.Description
+	}
+}
+
+// Variants
+type createVariantDTO struct {
+	SKU       string  `json:"sku" validate:"required,min=2,max=50"`
+	Color     string  `json:"color" validate:"omitempty,min=1,max=30"`
+	Size      string  `json:"size" validate:"omitempty,min=1,max=30"`
+	Price     float64 `json:"price" validate:"required,gt=0"`
+	Stock     int     `json:"stock" validate:"required,min=0"`
+	ProductID uint    `json:"productId" validate:"required"`
+}
+
+func (v *createVariantDTO) ToModel() model.Variant {
+	return model.Variant{
+		SKU:       v.SKU,
+		Color:     v.Color,
+		Size:      v.Size,
+		Price:     v.Price,
+		Stock:     v.Stock,
+		ProductID: v.ProductID,
+	}
+}
+
+func (dto *updateVariantDTO) ApplyModel(variant *model.Variant) {
+	if dto.Color != nil {
+		variant.Color = *dto.Color
+	}
+	if dto.Size != nil {
+		variant.Size = *dto.Size
+	}
+	if dto.Price != nil {
+		variant.Price = *dto.Price
+	}
+	if dto.Stock != nil {
+		variant.Stock = *dto.Stock
+	}
+}
