473f30a0fe07d5e0f55c810b640691a76448420f
Yusuf <easyclick06@gmail.com>
2025-08-30 13:02:55 +0100

    feat(route): register filter api and change update http method to  `PATCH`

--- api/internal/routes/product.go
+++ api/internal/routes/product.go
@@ -8,17 +8,19 @@ import (
 func registerProductRoutes(router chi.Router, productHandler interfaces.ProductHandler) {
 
 	router.Route("/products", func(r chi.Router) {
+		r.Get("/", productHandler.Filter)
+
 		r.Post("/", productHandler.Create)
 
 		r.Get("/{id}", productHandler.Get)
 
-		r.Put("/{id}", productHandler.Update)
+		r.Patch("/{id}", productHandler.Update)
 
 		r.Delete("/{id}", productHandler.Delete)
 
 		r.Route("/{productId}/variants", func(r chi.Router) {
 			r.Post("/", productHandler.CreateVariant)
-			r.Put("/{id}", productHandler.UpdateVariant)
+			r.Patch("/{id}", productHandler.UpdateVariant)
 			r.Delete("/{id}", productHandler.DeleteVariant)
 			r.Get("/{id}", productHandler.GetVariant)
 		})
