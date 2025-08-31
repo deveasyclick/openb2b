82acd07d93020dcebad3cffa34c02fbe579b51c9
Yusuf <easyclick06@gmail.com>
2025-08-31 22:27:22 +0100

    feat(order): add repository

--- api/internal/modules/order/respository.go
+++ api/internal/modules/order/respository.go
@@ -1,18 +1,18 @@
 package order
 
 import (
-	"github.com/deveasyclick/openb2b/internal/db"
+	"github.com/deveasyclick/openb2b/internal/factory"
 	"github.com/deveasyclick/openb2b/internal/model"
 	"github.com/deveasyclick/openb2b/pkg/interfaces"
 	"gorm.io/gorm"
 )
 
 type repository struct {
-	*db.BaseRepository[model.Order]
+	*factory.BaseRepository[model.Order]
 }
 
 func NewRepository(database *gorm.DB) interfaces.OrderRepository {
 	return &repository{
-		BaseRepository: db.NewBaseRepository[model.Order](database),
+		BaseRepository: factory.NewBaseRepository[model.Order](database),
 	}
 }
