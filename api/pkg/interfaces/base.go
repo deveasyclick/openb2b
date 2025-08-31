be749f1add936c72d20780d34e824b2ea662e363
Yusuf <easyclick06@gmail.com>
2025-08-31 11:03:18 +0100

    feat(interface): add base repository

---
+++ api/pkg/interfaces/base.go
@@ -0,0 +1,18 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/deveasyclick/openb2b/internal/shared/pagination"
+	"gorm.io/gorm"
+)
+
+type Repository[T any] interface {
+	Create(ctx context.Context, model *T) error
+	Update(ctx context.Context, model *T) error
+	FindByID(ctx context.Context, ID uint) (*T, error)
+	Filter(ctx context.Context, opts pagination.Options) ([]T, int64, error)
+	Delete(ctx context.Context, ID uint) error
+	FindOneWithFields(ctx context.Context, fields []string, where map[string]any, preloads []string) (*T, error)
+	WithTx(tx *gorm.DB) Repository[T]
+}
