4d9d9d7d97d717c951d70f288e69aad23589b0c7
Yusuf <easyclick06@gmail.com>
2025-08-31 22:26:55 +0100

    feat(interfaces): add base service and order handler and service

--- api/pkg/interfaces/base.go
+++ api/pkg/interfaces/base.go
@@ -16,3 +16,13 @@ type Repository[T any] interface {
 	FindOneWithFields(ctx context.Context, fields []string, where map[string]any, preloads []string) (*T, error)
 	WithTx(tx *gorm.DB) Repository[T]
 }
+
+type BaseService[T any] interface {
+	Create(ctx context.Context, model *T) error
+	Update(ctx context.Context, model *T) error
+	Delete(ctx context.Context, ID uint) error
+	FindByID(ctx context.Context, ID uint) (*T, error)
+	FindOneWithFields(ctx context.Context, fields []string, where map[string]any, preloads []string) (*T, error)
+	Filter(ctx context.Context, opts pagination.Options) ([]T, int64, error)
+	WithTx(tx *gorm.DB) BaseService[T]
+}
--- api/pkg/interfaces/order.go
+++ api/pkg/interfaces/order.go
@@ -1,7 +1,24 @@
 package interfaces
 
-import "github.com/deveasyclick/openb2b/internal/model"
+import (
+	"context"
+	"net/http"
 
+	"github.com/deveasyclick/openb2b/internal/model"
+)
+
+type OrderHandler interface {
+	Create(w http.ResponseWriter, r *http.Request)
+	Update(w http.ResponseWriter, r *http.Request)
+	Delete(w http.ResponseWriter, r *http.Request)
+	Get(w http.ResponseWriter, r *http.Request)
+	Filter(w http.ResponseWriter, r *http.Request)
+}
+
+type OrderService interface {
+	BaseService[model.Order]
+	Exists(ctx context.Context, where map[string]any) (bool, error)
+}
 type OrderRepository interface {
 	Repository[model.Order]
 }
