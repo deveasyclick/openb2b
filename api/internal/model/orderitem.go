f98167c1949cfbbbaf6356e4bb05f021351b1bdb
Yusuf <easyclick06@gmail.com>
2025-09-01 11:51:09 +0100

    feat(model): update order and order items model

--- api/internal/model/orderitem.go
+++ api/internal/model/orderitem.go
@@ -3,13 +3,13 @@ package model
 type OrderItem struct {
 	BaseModel
 
-	OrderID   uint    `json:"orderId"`
-	ProductID uint    `json:"productId"`
-	Product   Product `gorm:"foreignKey:ProductID" json:"product"`
-	VariantID uint    `json:"variantId"`
-	Variant   Variant `gorm:"foreignKey:VariantID" json:"variant"`
-	Quantity  int     `json:"quantity"`
-	UnitPrice float64 `json:"unitPrice"`
-	Total     float64 `json:"total"`
-	OrgID     uint    `json:"orgId"`
+	OrderID   uint     `json:"orderId"`
+	ProductID uint     `json:"productId"`
+	Product   *Product `gorm:"foreignKey:ProductID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	VariantID uint     `json:"variantId"`
+	Variant   *Variant `gorm:"foreignKey:VariantID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
+	Quantity  int      `json:"quantity"`
+	UnitPrice float64  `json:"unitPrice"`
+	Total     float64  `json:"total"`
+	OrgID     uint     `json:"orgId"`
 }
