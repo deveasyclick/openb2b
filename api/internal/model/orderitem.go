246381ad7d7850554c42bdf698178b4f58667530
Yusuf <easyclick06@gmail.com>
2025-09-02 11:41:00 +0100

    feat(orderitem): make variant id unique per order by adding unique composite index

--- api/internal/model/orderitem.go
+++ api/internal/model/orderitem.go
@@ -3,13 +3,15 @@ package model
 type OrderItem struct {
 	BaseModel
 
-	OrderID   uint     `json:"orderId"`
+	OrderID   uint     `json:"orderId" gorm:"uniqueIndex:idx_order_variant"`
 	ProductID uint     `json:"productId"`
 	Product   *Product `gorm:"foreignKey:ProductID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
-	VariantID uint     `json:"variantId"`
+
+	VariantID uint     `gorm:"uniqueIndex:idx_order_variant" json:"variantId"`
 	Variant   *Variant `gorm:"foreignKey:VariantID;constraint:OnUpdate:CASCADE,OnDelete:RESTRICT;"`
-	Quantity  int      `json:"quantity"`
-	UnitPrice float64  `json:"unitPrice"`
-	Total     float64  `json:"total"`
-	OrgID     uint     `json:"orgId"`
+
+	Quantity  int     `json:"quantity"`
+	UnitPrice float64 `json:"unitPrice"`
+	Total     float64 `json:"total"`
+	OrgID     uint    `json:"orgId"`
 }
