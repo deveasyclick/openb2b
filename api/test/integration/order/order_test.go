6eec77b1121f5c5921f6a3b0556942c2f37ee266
Yusuf <easyclick06@gmail.com>
2025-09-02 11:47:05 +0100

    feat(test): add order integration tests

--- api/test/integration/order/order_test.go
+++ api/test/integration/order/order_test.go
@@ -8,10 +8,10 @@ import (
 	"testing"
 
 	"github.com/deveasyclick/openb2b/internal/model"
-	"github.com/deveasyclick/openb2b/internal/modules/order"
+	"github.com/deveasyclick/openb2b/internal/shared/dto"
 	"github.com/deveasyclick/openb2b/internal/shared/response"
+	"github.com/deveasyclick/openb2b/test/integration/seed"
 	"github.com/deveasyclick/openb2b/test/integration/setup"
-	"github.com/deveasyclick/openb2b/test/integration/setup/seed"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,22 +20,26 @@ func TestOrderHandlers(t *testing.T) {
 	defer ts.Close()
 
 	db := setup.SetupTestDB()
-	seed.Clear(db) // reset DB for controlled testing
-	Insert(db)
+	seed.ClearOrders(db) // reset DB for controlled testing
+	seed.InsertOrders(db)
+
+	seed.ClearProducts(db)
+	seed.InsertProducts(db)
 	// -------------------- CREATE ORDER --------------------
 	t.Run("Create order - success", func(t *testing.T) {
-		reqBody := order.CreateOrderDTO{
+		reqBody := dto.CreateOrderDTO{
 			CustomerID: 1,
-			Items: []order.CreateOrderItemDTO{
-				{VariantID: 1, Quantity: 1, UnitPrice: 10.0, ProductID: 1},
+			Items: []dto.CreateOrderItemDTO{
+				{VariantID: seed.Product.Variants[0].ID, Quantity: 1},
+				{VariantID: seed.Product.Variants[1].ID, Quantity: 3},
 			},
-			Delivery: order.CreateDeliveryInfoDTO{
+			Delivery: dto.CreateDeliveryInfoDTO{
 				Address:       &model.Address{Address: "Street 1", City: "City 1", State: "State 1", Country: "Country 1"},
 				TransportFare: 10.0,
 			},
 			Notes: "Notes",
-			Discount: order.CreateDiscountInfoDTO{
-				Type:   model.DiscountPercentage,
+			Discount: dto.CreateDiscountInfoDTO{
+				Type:   model.DiscountFixed,
 				Amount: 3,
 			},
 		}
@@ -57,8 +61,62 @@ func TestOrderHandlers(t *testing.T) {
 		assert.Equal(t, http.StatusCreated, order.Code)
 		assert.Equal(t, order.Message, "success")
 
+		subtotal, discountAmount, taxAmount, total := calculateTotals(order.Data)
+		assert.Equal(t, order.Data.Total, total)
+		assert.Equal(t, order.Data.Subtotal, subtotal)
+		assert.Equal(t, order.Data.DiscountAmount, discountAmount)
+		assert.Equal(t, order.Data.Tax, taxAmount)
 		assert.Equal(t, int(order.Data.CustomerID), 1)
 		assert.Equal(t, int(order.Data.Discount.Amount), 3)
+		assert.Equal(t, order.Data.Discount.Type, model.DiscountFixed)
+		assert.Equal(t, float64(order.Data.Delivery.TransportFare), 10.0)
+		assert.Equal(t, order.Data.Delivery.Address.Address, "Street 1")
+		assert.Equal(t, order.Data.Delivery.Address.City, "City 1")
+		assert.Equal(t, order.Data.Delivery.Address.State, "State 1")
+		assert.Equal(t, order.Data.Delivery.Address.Country, "Country 1")
+		assert.Equal(t, order.Data.Notes, "Notes")
+		assert.Equal(t, len(order.Data.Items), 2)
+		assert.Equal(t, order.Data.Items[0].Quantity, 1)
+		assert.Equal(t, order.Data.Items[0].UnitPrice, 10.0)
+		assert.Equal(t, int(order.Data.Items[0].VariantID), 100)
+	})
+
+	t.Run("Create order - (zero discount) success", func(t *testing.T) {
+		reqBody := dto.CreateOrderDTO{
+			CustomerID: 1,
+			Items: []dto.CreateOrderItemDTO{
+				{VariantID: 99, Quantity: 1},
+			},
+			Delivery: dto.CreateDeliveryInfoDTO{
+				Address:       &model.Address{Address: "Street 1", City: "City 1", State: "State 1", Country: "Country 1"},
+				TransportFare: 10.0,
+			},
+			Notes: "Notes",
+			Discount: dto.CreateDiscountInfoDTO{
+				Type:   model.DiscountPercentage,
+				Amount: 0,
+			},
+		}
+		body, _ := json.Marshal(reqBody)
+		resp, err := http.Post(ts.URL+"/api/v1/orders", "application/json", bytes.NewBuffer(body))
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.NoError(t, err)
+		defer resp.Body.Close()
+		assert.Equal(t, http.StatusCreated, resp.StatusCode)
+
+		var order response.APIResponse[model.Order]
+		err = json.NewDecoder(resp.Body).Decode(&order)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusCreated, order.Code)
+		assert.Equal(t, order.Message, "success")
+
+		assert.Equal(t, int(order.Data.CustomerID), 1)
+		assert.Equal(t, int(order.Data.Discount.Amount), 0)
 		assert.Equal(t, order.Data.Discount.Type, model.DiscountPercentage)
 		assert.Equal(t, float64(order.Data.Delivery.TransportFare), 10.0)
 		assert.Equal(t, order.Data.Delivery.Address.Address, "Street 1")
@@ -68,13 +126,14 @@ func TestOrderHandlers(t *testing.T) {
 		assert.Equal(t, order.Data.Notes, "Notes")
 		assert.Equal(t, len(order.Data.Items), 1)
 		assert.Equal(t, order.Data.Items[0].Quantity, 1)
-		assert.Equal(t, order.Data.Items[0].UnitPrice, 10.0)
-		assert.Equal(t, int(order.Data.Items[0].VariantID), 1)
+		assert.Equal(t, order.Data.Items[0].UnitPrice, 20.0)
+		assert.Equal(t, int(order.Data.Items[0].VariantID), 99)
+		assert.Equal(t, int(order.Data.Total), 20)
 	})
 
 	t.Run("Create order - missing name (400)", func(t *testing.T) {
-		reqBody := order.CreateOrderDTO{
-			Items: []order.CreateOrderItemDTO{
+		reqBody := dto.CreateOrderDTO{
+			Items: []dto.CreateOrderItemDTO{
 				{VariantID: 1, Quantity: 1},
 			},
 		}
@@ -104,6 +163,38 @@ func TestOrderHandlers(t *testing.T) {
 	})
 
 	// -------------------- UPDATE PRODUCT --------------------
+
+	t.Run("Update order - (replace order items) success", func(t *testing.T) {
+		reqBody := map[string]any{"Notes": "This is a note", "Items": []map[string]any{{"Quantity": 3, "VariantId": seed.Product.Variants[0].ID}, {"Quantity": 3, "VariantId": seed.Product.Variants[1].ID}}}
+		body, _ := json.Marshal(reqBody)
+		req, _ := http.NewRequest(http.MethodPatch, ts.URL+"/api/v1/orders/1", bytes.NewBuffer(body))
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		assert.NoError(t, err)
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		var order response.APIResponse[model.Order]
+		err = json.NewDecoder(resp.Body).Decode(&order)
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.NoError(t, err)
+
+		assert.Equal(t, order.Data.Notes, "This is a note")
+
+		subtotal, discountAmount, taxAmount, total := calculateTotals(order.Data)
+		assert.Equal(t, order.Data.Total, total)
+		assert.Equal(t, order.Data.Subtotal, subtotal)
+		assert.Equal(t, order.Data.DiscountAmount, discountAmount)
+		assert.Equal(t, order.Data.Tax, taxAmount)
+		assert.Equal(t, len(order.Data.Items), 2)
+		assert.Equal(t, order.Data.Items[0].Quantity, 3)
+		assert.NotEqual(t, order.Data.Items[0].VariantID, 1)
+	})
+
 	t.Run("Update order - success", func(t *testing.T) {
 		reqBody := map[string]any{"Notes": "This is a note"}
 		body, _ := json.Marshal(reqBody)
@@ -115,6 +206,30 @@ func TestOrderHandlers(t *testing.T) {
 		defer resp.Body.Close()
 
 		assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+		var order response.APIResponse[model.Order]
+		err = json.NewDecoder(resp.Body).Decode(&order)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.NoError(t, err)
+		assert.Equal(t, order.Data.Notes, "This is a note")
+		assert.Equal(t, order.Data.OrderNumber, "ORD-123")
+		assert.Empty(t, order.Data.Items)
+	})
+
+	t.Run("Update order - (duplicate order items) success", func(t *testing.T) {
+		reqBody := map[string]any{"Notes": "This is a note", "Items": []map[string]any{{"Quantity": 3, "VariantId": seed.Product.Variants[1].ID}, {"Quantity": 2, "VariantId": seed.Product.Variants[1].ID}}}
+		body, _ := json.Marshal(reqBody)
+		req, _ := http.NewRequest(http.MethodPatch, ts.URL+"/api/v1/orders/1", bytes.NewBuffer(body))
+		req.Header.Set("Content-Type", "application/json")
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		assert.NoError(t, err)
+		defer resp.Body.Close()
+
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode, "response status code")
 	})
 
 	t.Run("Update order - invalid ID (400)", func(t *testing.T) {
@@ -131,7 +246,7 @@ func TestOrderHandlers(t *testing.T) {
 	t.Run("Update order - not in pending status (400)", func(t *testing.T) {
 		reqBody := map[string]any{"Notes": "Updated Note"}
 		body, _ := json.Marshal(reqBody)
-		req, _ := http.NewRequest(http.MethodPatch, fmt.Sprintf("%s/api/v1/orders/%d", ts.URL, nonPendingOrderId), bytes.NewBuffer(body))
+		req, _ := http.NewRequest(http.MethodPatch, fmt.Sprintf("%s/api/v1/orders/%d", ts.URL, seed.NonPendingOrderId), bytes.NewBuffer(body))
 		req.Header.Set("Content-Type", "application/json")
 		client := &http.Client{}
 		resp, err := client.Do(req)
